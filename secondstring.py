955f6b91d7fb8eae47321a5b3e604a3151bc972e
Torsten <98063103+Torsten-GMIT@users.noreply.github.com>
2022-03-17 03:41:06 +0000

    task wk03

--- secondstring.py
+++ secondstring.py
@@ -4,3 +4,10 @@
  # A program that asks a user to input a string and outputs every second letter in reverse order.
 
 
+string = input("Enter a string: ")
+outString = (string[::-1])
+
+print("Your string is '{}'. Every second letter in reverse order of that string is '{}'.".format(string, outString[::2]))
+
+
+# https://stackoverflow.com/questions/663171/how-do-i-get-a-substring-of-a-string-in-python and https://www.w3schools.com/python/python_howto_reverse_string.asp
