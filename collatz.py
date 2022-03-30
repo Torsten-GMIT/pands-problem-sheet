ee998a3abc37c4d01a8c7a9225b4536893dffb5a
Torsten <98063103+Torsten-GMIT@users.noreply.github.com>
2022-03-30 15:24:45 +0100

    task wk04

---
+++ collatz.py
@@ -0,0 +1,17 @@
+# Task week 04
+# Author: Torsten Kindt
+
+# A program that asks a user to input a positive Integer and outputs the successive values of the Collatz Conjecture, otherwise known as the 3x+1 problem: At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. 
+# Have the program end if the current value is one.
+
+
+num = int(input("Enter a positive integer:"))
+
+while (num > 1):
+    print(int(num))
+    if (num % 2) == 0:
+        num = (num / 2)
+    else:
+        num = ((num*3) + 1)
+print(1,"\nThe End")
+
